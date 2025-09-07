39d03e8abf84a298689130d6a84c2941b181a59b
Anand Jha <anandkumarj669@gmail.com>
2025-09-07 13:50:21 +0530

    1:50

--- inngest/client.js
+++ inngest/client.js
@@ -4,7 +4,7 @@ import { INNGEST_EVENT_KEY , INNGEST_SIGNING_KEY} from "../configs/server.config
 
 export const inngest = new Inngest({
   id: "ticketing-system",
-  eventKey: INNGEST_EVENT_KEY,
-  signingKey: INNGEST_SIGNING_KEY,
+  eventKey: process.env.INNGEST_EVENT_KEY,
+  signingKey: Iprocess.env.INNGEST_SIGNING_KEY,
   environment: process.env.NODE_ENV === "production" ? "prod" : "dev",
 });
