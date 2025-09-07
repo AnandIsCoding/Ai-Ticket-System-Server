049008f693d04c1b8ec3792624d4156844f28212
Anand Jha <anandkumarj669@gmail.com>
2025-09-07 13:44:43 +0530

    still trying to fix inngest issue

--- inngest/client.js
+++ inngest/client.js
@@ -5,5 +5,6 @@ import { INNGEST_EVENT_KEY , INNGEST_SIGNING_KEY} from "../configs/server.config
 export const inngest = new Inngest({
   id: "ticketing-system",
   eventKey: INNGEST_EVENT_KEY,
-  signingKey: INNGEST_SIGNING_KEY
+  signingKey: INNGEST_SIGNING_KEY,
+  environment: process.env.NODE_ENV === "production" ? "prod" : "dev",
 });
